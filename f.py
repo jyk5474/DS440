a5f518e8bc02d3fe5cc6d31b0c4967e13a0ffca3
JackKociolek <97631132+jyk5474@users.noreply.github.com>
2024-11-10 13:48:29 -0500

    Update f.py

--- f.py
+++ f.py
@@ -1,8 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import subprocess
+
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-   return render_template('index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate_graph', methods=['POST'])
+def generate_graph():
+    data = request.get_json()
+    zip_code = data.get('zipCode')
+    demographic_type = data.get('demographicType')
+
+
+    graph_filename = f"{demographic_type}_{zip_code}.png"
+    script_name = 'generate_demographic_graph.py'
+    
+    subprocess.run(['python', script_name, zip_code, demographic_type, f'static/graphs/{graph_filename}'])
+
+    return jsonify(success=True, graphFilename=graph_filename)
+
 if __name__ == '__main__':
-   app.run()
+    app.run(debug=True)
