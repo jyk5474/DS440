ac540f1feef1b99b0fb498b495ad9e7e89e79044
JackKociolek <97631132+jyk5474@users.noreply.github.com>
2024-11-10 15:54:58 -0500

    Added host and port for render deployment

--- f.py
+++ f.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import subprocess
+import os
 
 app = Flask(__name__)
 
@@ -13,13 +14,16 @@ def generate_graph():
     zip_code = data.get('zipCode')
     demographic_type = data.get('demographicType')
 
-
     graph_filename = f"{demographic_type}_{zip_code}.png"
     script_name = 'generate_demographic_graph.py'
     
+    # Make sure the graphs directory exists
+    os.makedirs('static/graphs', exist_ok=True)
+    
     subprocess.run(['python', script_name, zip_code, demographic_type, f'static/graphs/{graph_filename}'])
-
     return jsonify(success=True, graphFilename=graph_filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Get port from environment variable for Render deployment
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
